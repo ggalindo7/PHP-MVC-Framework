924304d93ab5188719e60b64f0406f70d24f4bd9
GG <g.galindo724@gmail.com>
2020-02-06 12:53:02 -0800

    Initial commit

---
+++ app/views/pages/index.php
@@ -0,0 +1,4 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h1><?php echo $data['title'];?></h1>
+<p>A light PHP MVC Framework. Please refer to docs to get started.</p>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
